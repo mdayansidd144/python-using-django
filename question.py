bc690fdad8d4f5cabe7d385080f93854e3774f10
Mohd Ayan Siddiqui <ayansiddiqui1411@gmail.com>
2026-02-03 15:23:18 +0530

    solve the problem

---
+++ question.py
@@ -0,0 +1,37 @@
+# x=int(input("enter the number"))
+# y = int(input("enter the second number"))
+# sum = x+y,
+# difference = x-y,
+# product =x*y,
+# quotient= x/y
+# print(sum,difference,product,quotient)
+# a = 15
+# b = 4
+# floordivison  = (a//b)
+# remainder = (a%b)
+# power = a**b
+# print(floordivison,remainder,power)
+# x = 15
+# y = 40
+# totalcost = 3*x + 2*y
+# print(totalcost)
+# c = int(input("enter the first num"))
+# d = int(input("enter the sec num"))
+# e = int(input("enter the third num"))
+# f = int(input("enter the fourth num"))
+# g = int(input("enter the fifth num"))
+# avg = ((c+d+e+f+g)/5)
+# print(avg)
+# i = int(input("enter the number"))
+# sqr = i**2
+# print(sqr)
+# j = int(input("enter the number"))
+# cube = j**3
+# print(cube)
+h = int(input("enter the num"))
+prog = (h//60)+(h%60)
+print(prog)
+x = 120
+y = 3
+speed = x/y
+print(speed)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+j = int(input("enter the number"))
+# cube = j**3
+# print(cube)
+# h = int(input("enter the num"))
+# prog = h//60+h%60
