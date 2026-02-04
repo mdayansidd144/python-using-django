bd8f6e8855926b283afabfd17796085a48ededd4
Mohd Ayan Siddiqui <ayansiddiqui1411@gmail.com>
2026-02-04 15:29:03 +0530

    add the question

--- question.py
+++ question.py
@@ -134,14 +134,20 @@
 #   print("valid")
 # else :
 #   print("invalid")  
-a = int(input("enter the first marks"))
-y = int(input("enter the second marks"))
-z = int(input("enter the third marks"))
-if a >=35 and y>=35 and z>=35:
-  print("passed")
-elif a>=35 and y<35 and z<35:
-  print("pass is one")
-elif a>=35 and y>=35 and z<35:
-  print("passed in two")    
-else:
-  print("fail in all")  
+# a = int(input("enter the first marks"))
+# y = int(input("enter the second marks"))
+# z = int(input("enter the third marks"))
+# if a >=35 and y>=35 and z>=35:
+#   print("passed")
+# elif a>=35 and y<35 and z<35:
+#   print("pass is one")
+# elif a>=35 and y>=35 and z<35:
+#   print("passed in two")    
+# else:
+#   print("fail in all")  
+
+
+# project overview
+# tools and technology
+# industry presentation
+# future scope
